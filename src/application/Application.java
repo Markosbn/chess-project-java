d7e74203d233791f4d23ce42e6da839c9e0a52ac
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-03-04 08:59:43 -0300

    Adicionado limpeza de console, e excessão em tela

--- src/application/Application.java
+++ src/application/Application.java
@@ -3,7 +3,9 @@ package application;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import chess.exceptions.ChessException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
@@ -14,16 +16,24 @@ public class Application {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }catch (ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
